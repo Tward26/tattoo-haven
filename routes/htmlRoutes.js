0cf16436d7f48afc8eae32f60df0e1ddee0fc0b4
Benita Jones <47437893+b3nitajo@users.noreply.github.com>
2019-06-04 20:30:09 -0500

    routesAdded

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,24 +1,32 @@
 var db = require('../models');
+var path = require("path");
 
 module.exports = function(app) {
+
   // Load index page
   app.get('/', function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render('index', {
-        msg: 'Welcome!',
-        examples: dbExamples
+     res.render('index');
+  });
+
+  // Load artist page and pass in an artist by id
+  app.get('/artist/:id', function(req, res) {
+    db.artist.findOne({ where: { id: req.params.id } }).then(function(dbArtist) {
+      res.render('artist', {
+        id: dbArtist
       });
     });
   });
 
-  // Load example page and pass in an example by id
-  app.get('/example/:id', function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render('example', {
-        example: dbExample
-      });
-    });
+  // Load about page
+  app.get('/', function(req, res) {
+    res.render('about');
   });
+
+   // Load reseveration page
+   app.get('/', function(req, res) {
+      res.render('reservations');
+    });
+  
 
   // Render 404 page for any unmatched routes
   app.get('*', function(req, res) {
